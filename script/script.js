60a277f734c952313fb7eb8e937faf71bbd0b41a
Gmbvt <AlvGkFoZpR@outlook.com>
2026-08-21 17:45:48 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function openTab(evt, onglet) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(onglet).style.display = "flex";
+    evt.currentTarget.className += " active";
+  } 
+
